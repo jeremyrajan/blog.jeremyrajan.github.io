84a6e9d70edb65fcc974803e7b58d9a7912819e5
jeremyrajan <jeremyrajan@gmail.com>
2016-04-28 00:32:31 +0800

    init commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function changeHeart(elem) {
+  for(var i=0;i<elem.length;i++) {
+    var title = $(elem[i]).text();
+    if (title.indexOf('<3') > -1) {
+      $(elem[i]).html(title.replace('<3', '<i class="fa fa-heart" aria-hidden="true"></i>'));
+    }
+  }
+}
+$(document).ready( function () {
+  changeHeart($('.post-link'));
+  changeHeart($('.post-title'));
+});
